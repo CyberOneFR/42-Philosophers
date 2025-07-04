9c035e8effd38094247469832934d1beb50f0918
CyberOneFR <erwoan.thebault@gmail.com>
2025-07-04 23:09:04 +0200

    limits fix

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,11 +6,36 @@
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 09:48:25 by ethebaul          #+#    #+#             */
-/*   Updated: 2025/07/03 23:55:27 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/07/04 23:08:56 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	limits(t_settings *set, int philo)
+{
+	if (philo > 200)
+	{
+		printf("too many philosophers\n");
+		return (1);
+	}
+	if (set->die > 1000000)
+	{
+		printf("time to die too big\n");
+		return (1);
+	}
+	if (set->eat > 1000000)
+	{
+		printf("time to eat too big\n");
+		return (1);
+	}
+	if (set->sleep > 1000000)
+	{
+		printf("time to sleep too big\n");
+		return (1);
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -32,6 +57,8 @@ int	main(int argc, char **argv)
 		write(2, "invalid arguments\n", 19);
 		return (1);
 	}
+	if (limits(&set, philo))
+		return (1);
 	if (sim_start(&set, philo))
 		return (1);
 	return (0);
